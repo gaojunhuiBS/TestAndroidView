c920f918ff8d3e46bf36a6878b038d7d39768ff1
pix <tpxsky@163.com>
2017-03-24 13:24:20 +0800

    add AppBarLayout test demo

--- AppBarLayoutDemo/app/src/main/java/com/pix/appbarlayoutdemo/MainActivity.java
+++ AppBarLayoutDemo/app/src/main/java/com/pix/appbarlayoutdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.pix.appbarlayoutdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View v) {
+        Button btn = (Button) v;
+        if(btn.getText().equals("Scroll Mode")) {
+            startActivity(new Intent(this,AppBarLayoutScrollActivity.class));
+        }
+
+        if(btn.getText().equals("Scroll | EnterAlways Mode")) {
+            startActivity(new Intent(this,ScrollEnterAlwaysActivity.class));
+        }
+
+        if(btn.getText().equals("Scroll | ExitUntilCollapsed")) {
+            startActivity(new Intent(this,ScrollExitUntilCollapsedActivity.class));
+        }
+
+        if(btn.getText().equals("Scroll|EnerAlways|enterAlwaysCollapsed")) {
+            startActivity(new Intent(this,EnterAlwaysCollapsedActivity.class));
+        }
+
+    }
+
 }
